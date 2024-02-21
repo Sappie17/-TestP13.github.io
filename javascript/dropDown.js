e724d5daa4e4871f331ed6b3c784b3865f3023f6
Sappie17 <156705072+Sappie17@users.noreply.github.com>
2024-02-21 19:07:07 +0100

    Delete javascript directory

--- javascript/dropDown.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const hamburgerIcon = document.getElementById('hamburger-icon');
-    const nav = document.querySelector('nav');
-
-    hamburgerIcon.addEventListener('click', function () {
-    nav.classList.toggle('active');
-    });
-    });
